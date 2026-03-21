894a524c549441e44c1147316f644fd71e1edfe2
Matt Vasquez <matthew.aaron.vasquez@gmail.com>
2026-03-21 18:41:00 -0400

    fixed tests

--- tests/test_parse_team_ids.py
+++ tests/test_parse_team_ids.py
@@ -3,7 +3,7 @@ import logging
 from nba_game_predictor.statistics import Statistics
 
 _LOGGER = logging.getLogger(__name__)
-def test_get_team_ids_returns_list_of_dicts():
+def test_parse_team_ids_returns_list_of_dicts():
     statistics = Statistics()
     """ Use file w/ test json and loop through test cases and compare to expected results """
 
@@ -15,7 +15,7 @@ def test_get_team_ids_returns_list_of_dicts():
         _LOGGER.warning("test_event(): Error with test file '%s'", "tests/test_data/nba_teams.json")
         assert False
     
-    result = statistics.get_team_ids(data)
+    result = statistics.parse_team_ids(data)
     
         # Assert
     assert isinstance(result, list)
